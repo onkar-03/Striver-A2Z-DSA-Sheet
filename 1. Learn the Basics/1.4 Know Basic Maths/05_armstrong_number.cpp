50871a0d20e7fb9581a307113c2096469033250b
Onkar Patel <onkarpatel10@gmail.com>
2024-07-18 22:10:23 +0530

    Corrected armstrong number code

--- 1. Learn the Basics/1.4 Know Basic Maths/05_armstrong_number.cpp
+++ 1. Learn the Basics/1.4 Know Basic Maths/05_armstrong_number.cpp
@@ -2,70 +2,35 @@
 using namespace std;
 
 // Function to find the Highest Common Factor (HCF) using the Brute Force method
-int HCFBruteForce(int number1, int number2)
+bool isArmstrongNumber(int number)
 {
-  int end = min(number1, number2); // Determine the smaller number between number1 and number2
-
-  int hcf = 1;                   // Default HCF is 1
-  for (int i = 1; i <= end; i++) // Iterate from 1 to the smaller number [O(min(number1, number2))]
+  int sum = 0;
+  int originalNumber = number;
+  while (originalNumber != 0)
   {
-    if (number1 % i == 0 && number2 % i == 0)
-    {
-      hcf = i; // Update HCF if both numbers are divisible by i
-    }
+    int digit = originalNumber % 10;
+    sum += pow(digit, 3);
+    originalNumber /= 10;
   }
-  return hcf; // Return the computed HCF
-}
-
-// Function to find the Highest Common Factor (HCF) using the Euclidean Algorithm
-int HCFEuclideanAlgorithm(int number1, int number2)
-{
-  while (number1 > 0 && number2 > 0)
-  {
-    if (number1 > number2)
-      number1 = number1 % number2; // Update number1 to remainder of number1 divided by number2
-    else if (number2 > number1)
-      number2 = number2 % number1; // Update number2 to remainder of number2 divided by number1
-  }
-  if (number1 == 0)
-  {
-    return number2; // Return number2 when number1 becomes zero
-  }
-  else
-  {
-    return number1; // Return number1 when number2 becomes zero
-  }
-
-  // Time Complexity: O(log(min(number1, number2)))
+  return (sum == number);
 }
 
 int main()
 {
-  int number1, number2;
+  int number;
 
   // Prompt the user to enter two numbers
-  cout << "Enter First Number: ";
-  cin >> number1;
-  cout << "Enter Second Number: ";
-  cin >> number2;
+  cout << "Enter the Number: ";
+  cin >> number;
 
-  // Determine the HCF using Euclidean Algorithm
-  int hcf = 1;
-  if (number1 > number2)
+  if (isArmstrongNumber(number))
   {
-    hcf = HCFEuclideanAlgorithm(number1, number2);
+    cout << number << " is an Armstrong Number";
   }
   else
   {
-    hcf = HCFEuclideanAlgorithm(number2, number1);
+    cout << number << " is not an Armstrong Number";
   }
-
-  // Calculate the LCM using the formula LCM(a, b) = (a * b) / HCF(a, b)
-  int lcm = number1 * number2 / hcf;
-
-  // Output the results
-  cout << "HCF of " << number1 << " and " << number2 << " is: " << hcf << endl;
-  cout << "LCM of " << number1 << " and " << number2 << " is: " << lcm << endl;
 
   return 0;
 }
